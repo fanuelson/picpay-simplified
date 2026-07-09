005ebf81efe9a8b06d134f9a5f164f2d4d47a5bc
ffn <fanuelson@gmail.com>
2026-07-09 15:33:17 -0300

    test: k6 test

---
+++ src/test/javascript/transfer1-2.test.js
@@ -0,0 +1,21 @@
+import {check} from 'k6';
+import http from 'k6/http';
+
+const host = 'localhost';
+const port = '8080';
+const endpoint = '/v1/transfer'
+
+export default function () {
+    let payload = JSON.stringify(
+        {
+            "payer": 1,
+            "payee": 2,
+            "amount": 1
+        }
+    );
+
+    let params = {headers: {"Content-Type": "application/json"}};
+    let res = http.post(`http://${host}:${port}${endpoint}`, payload, params);
+
+    check(res, {'is status 200': (r) => r.status === 200});
+}
